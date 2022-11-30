d0accab720c99752f0a85b60cb21ce765665bac8
CodingClowns <finlay.ryan.arthur.smith@gmail.com>
2022-11-30 14:57:04 -0800

    Updated Player logic
    +Added documentation to the PlayerMovement and GroundCheck classes.
    +Added slippery physics2D material. The player can no longer stick to walls.
    
    ~Fixed some typos.
    ~Changed ground detection to accept any collider that isn't the player's.
    ~Made ground detection trigger smaller.

--- FinalProject/Assets/Scripts/Player/GroundCheck.cs
+++ FinalProject/Assets/Scripts/Player/GroundCheck.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Detects ground collision and tells the player when they can jump.
+/// </summary>
 public class GroundCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private PlayerMovement playerMovement;
-
-    public void OnTriggerEnter2D(Collider2D collision)
+    [Header("References")]
+    [SerializeField] private PlayerMovement movement;
+    [SerializeField] private GameObject player;
+    
+    // GroundCheck
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Platform"))
+        if (!collision.gameObject.CompareTag("Player")) //The player can jump on anything except its own collider.
         {
-            playerMovement.IsOnGround();
-            Debug.Log("Onground!");
+            //Debug.Log("Player jumpable");
+            movement.IsOnGround();
         }
     }
 }
