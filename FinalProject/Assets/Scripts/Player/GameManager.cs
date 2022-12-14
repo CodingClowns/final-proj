43ba66b83850cc145d5f261e2e422bb71e494a19
CodingClowns <finlay.ryan.arthur.smith@gmail.com>
2022-12-14 00:11:17 -0800

    Final tweaks
    +Added pickup script to Coins.
    +Added Chao (Powerup) prefab.
    ~Chao movement is now handled by the FloatingPickup script.
    ~Skeletons now use the Enemy tag
    ~Moved LockDoor and PassiveHazard to World folder
    ~GameManager has been moved to the Manager scene to allow score to be managed in any zone.

--- FinalProject/Assets/Scripts/Player/GameManager.cs
+++ FinalProject/Assets/Scripts/Player/GameManager.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Holds and updates the player's score.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     public int score;
     [SerializeField] private Text textmeshPro;
-    void Start()
-    {
-        textmeshPro = FindObjectOfType<Text>();
-    }
+    
     void Update()
     {
         textmeshPro.text = "Score: " + score;
